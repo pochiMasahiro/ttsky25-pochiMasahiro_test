b4c01aa56d3839f90c90bba14de1f3487bfa1f26
MFukuoka <pochi.masahiro@gmail.com>
2025-08-17 01:23:14 +0900

    initial

--- src/tt_um_pochiMasahiro_test.v
+++ src/tt_um_pochiMasahiro_test.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_pochiMasahiro_test (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,23 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, ui_in[7], ui_in[6], ui_in[5], ui_in[4], ui_in[3], ui_in[2], ui_in[1], ui_in[0], uo_out[7], uo_out[6], uo_out[5], 1'b0};
+
+  wire [3:0] d;
+  assign uo_out[3:0] = d;
+
+  reg [3:0] count;
+
+  always @(posedge clk or negedge rst_n)
+  begin
+      if (!rst_n)
+          count <= 4'b0;
+      else
+          count <= count + 4'b1;
+  end
+
+  assign d = count;
 
 endmodule
